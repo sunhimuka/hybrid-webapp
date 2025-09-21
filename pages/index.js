6f0517f87128684081d504e082e9e6363963cd02
Yasunori Kodama <sunhimuka1@gmail.com>
2025-09-21 20:13:08 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: "sans-serif",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      minHeight: "100vh"
+    }}>
+      <h1>Hello, Hybrid WebApp 🚀</h1>
+      <p>Now powered by Next.js on Vercel 🎉</p>
+    </div>
+  )
+}
