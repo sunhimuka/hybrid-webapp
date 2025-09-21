d4200c2fd5245301c75b89d1026d94dcef1017d5
Yasunori Kodama <sunhimuka1@gmail.com>
2025-09-21 21:13:26 +0900

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,10 @@
-import Link from 'next/link';
+import Layout from "../components/Layout";
 
 export default function About() {
   return (
-    <div style={{
-      fontFamily: "sans-serif",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      background: "#e0f7fa"
-    }}>
+    <Layout>
       <h1>About Page</h1>
       <p>これは Next.js で作成した About ページです 🚀</p>
-
-      <p>
-        <Link href="/">🏠 ホームに戻る</Link>
-      </p>
-    </div>
+    </Layout>
   );
 }
